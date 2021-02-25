1017e8e8553dcda604a086e41df668d6a8c9939f
Umesh <umeshprajapati499@gmail>
2021-02-25 10:39:54 +0545

    Added Layout file

---
+++ jquery-js-files/jquery.js
@@ -0,0 +1,20 @@
+//custom jquery in magento 2
+require([
+  'jquery'
+], function ($) {
+	$(document).ready(function(){
+    //your code
+    });
+});
+
+//slick in custom js in magento 2
+require([
+  'jquery',
+  'slick'
+], function ($) {
+	$(document).ready(function(){
+	//code
+	console.log('test');
+	});
+});
+
